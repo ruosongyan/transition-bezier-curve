a0a542a87a5ec269a6504e1bc0cdd4accc982cca
ruosongyan <mf1732158@smail.nju.edu.cn>
2018-11-02 20:56:46 +0800

    重构viewmodel

--- viewmodel.js
+++ viewmodel.js
@@ -1,47 +1,79 @@
 class Observer{
     constructor(obj){
-        this.eventList={};
-        this._initReactive(obj);
+        this.observer(obj);
     }
-    on(key,event){
-        let eventList = this.eventList;
-        eventList[key]=eventList[key] || [];
-        if(event && event instanceof Watcher){
-            eventList[key].push(event);
-        }  
-    }
-    trigger(key,value){
-        let eventList = this.eventList;
-        if(eventList[key] && eventList[key].length>0){
-            for(let event of eventList[key]){
-                event.call(value);
-            }
+    observer(obj){
+        if(!obj || Object.prototype.toString.call(obj) != '[object Object]'){
+            return
+        }
+        for(let key of obj){
+            this._initReactive(obj[key]);
         }
     }
     _initReactive(obj){
         let that = this;
-        for(let key in obj){
-            let value = obj[key];
-            Object.defineProperty(obj,key,{
-                configurable:true,
-                enumerable:true,
-                get:function(){
-                    return value;
-                },
-                set:function(newValue){
-                    value = newValue;
-                    that.trigger(key,newValue);
+        if(Object.prototype.toString.call(obj[key]) == '[object Object]'){
+            this._initReactive(obj[key]);
+        }
+        //obj每一个属性都有一个Dep来管理订阅队列
+        let dep = new Dep();
+        let value = obj[key];
+        Object.defineProperty(obj,key,{
+            configurable:true,
+            enumerable:true,
+            get:function(){
+                //在第一次获取值得时候进行订阅
+                if(Dep.target){
+                    dep.on(Dep.target);
                 }
-            })
-            if(Object.prototype.toString.call(obj[key]) == '[object Object]'){
-                this._initReactive(obj[key]);
+                return value;
+            },
+            set:function(newValue){
+                value = newValue;
+                dep.notify();
             }
+        })
+    }
+}
+class Dep{
+    constructor(){
+        this.eventList=[];
+    }
+    on(event){
+        if(event && event instanceof Watcher){
+            this.eventList.push(event);
+        }
+    }
+    notify(){
+        for(let event of eventList){
+            event.update();
         }
     }
 }
 class Watcher{
-    constructor(action){
-        this.call = action;
+    constructor(data,exp,cb){
+        this.data = data;
+        this.exp = exp;
+        this.cb = cb;
+        this.value = this.get();
+    }
+    update(){
+        this.run();
+    }
+    run(){
+        let oldValue = this.value;
+        let newValue = this.data[this.exp];
+        if(oldValue != newValue){
+            this.value = newValue;
+            //----------cb绑定作用域------------
+            this.cb.call(this.data,newValue,oldValue);
+        }
+    }
+    get(){
+        Dep.target = this;
+        let value = this.data[exp];
+        Dep.target = null;
+        return value;
     }
 }
 
